e0a1fb5c1c58868db8fb608b2f5ba9d8addd249d
JailtonMS <eng.jailton@gmail.com>
2021-10-20 18:28:09 -0300

    Commit inicial

---
+++ _javascript/funcoes.js
@@ -0,0 +1,545 @@
+var selecnum1 = false
+var deselnum1 = false
+var selecnum2 = false
+var selecnum3 = false
+var selecnum4 = false
+var selecnum5 = false
+var selecnum6 = false
+var selecnum7 = false
+var selecnum8 = false
+var selecnum9 = false
+var selecnum10 = false
+var selecnum11 = false
+var selecnum12 = false
+var selecnum13 = false
+var selecnum14 = false
+var selecnum15 = false
+var selecnum16 = false
+var selecnum17 = false
+var selecnum18 = false
+var selecnum19 = false
+var selecnum20 = false
+var selecnum21 = false
+var selecnum22 = false
+var selecnum23 = false
+var selecnum24 = false
+var selecnum25 = false
+var SelCart = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+let valores = []
+var incpos = 0 /* Incremente posição*/
+
+function mudacor1(tipo){
+    if (!selecnum1){
+        document.getElementById("Num1").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_01" && deselnum1 == false){
+            selecnum1 = true
+            valores [incpos] = SelCart [0]
+            incpos++
+        }              
+    }
+}
+function mudacor2(tipo){
+    if (!selecnum2){
+        document.getElementById("Num2").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_02"){
+            selecnum2 = true
+            valores [incpos] = SelCart [1]
+            incpos++
+        }
+    }
+} 
+function mudacor3(tipo){
+    if (!selecnum3){
+        document.getElementById("Num3").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_03"){
+            selecnum3 = true
+            valores [incpos] = SelCart [2]
+            incpos++
+        }
+    }
+} 
+function mudacor4(tipo){
+    if (!selecnum4){
+        document.getElementById("Num4").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_04"){
+            selecnum4 = true
+            valores [incpos] = SelCart [3]
+            incpos++
+        }
+    }
+} 
+function mudacor5(tipo){
+    if (!selecnum5){
+        document.getElementById("Num5").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_05"){
+            selecnum5 = true
+            valores [incpos] = SelCart [4]
+            incpos++
+        }
+    }
+}
+function mudacor6(tipo){
+    if (!selecnum6){
+        document.getElementById("Num6").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_06"){
+            selecnum6 = true
+            valores [incpos] = SelCart [5]
+            incpos++
+        }
+    }
+}
+function mudacor7(tipo){
+    if (!selecnum7){
+        document.getElementById("Num7").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_07"){
+            selecnum7 = true
+            valores [incpos] = SelCart [6]
+            incpos++
+        }
+    }
+} 
+function mudacor8(tipo){
+    if (!selecnum8){
+        document.getElementById("Num8").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_08"){
+            selecnum8 = true
+            valores [incpos] = SelCart [7]
+            incpos++
+        }
+    }
+} 
+function mudacor9(tipo){
+    if (!selecnum9){
+        document.getElementById("Num9").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_09"){
+            selecnum9 = true
+            valores [incpos] = SelCart [8]
+            incpos++
+        }
+    }
+} 
+function mudacor10(tipo){
+    if (!selecnum10){
+        document.getElementById("Num10").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_10"){
+            selecnum10 = true
+            valores [incpos] = SelCart [9]
+            incpos++
+        }
+    }
+}
+function mudacor11(tipo){
+    if (!selecnum11){
+        document.getElementById("Num11").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_11"){
+            selecnum11 = true
+            valores [incpos] = SelCart [10]
+            incpos++
+        }
+    }
+}
+function mudacor12(tipo){
+    if (!selecnum12){
+        document.getElementById("Num12").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_12"){
+            selecnum12 = true
+            valores [incpos] = SelCart [11]
+            incpos++
+        }
+    }
+}
+function mudacor13(tipo){
+    if (!selecnum13){
+        document.getElementById("Num13").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_13"){
+            selecnum13 = true
+            valores [incpos] = SelCart [12]
+            incpos++
+        }
+    }
+}
+function mudacor14(tipo){
+    if (!selecnum14){
+        document.getElementById("Num14").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_14"){
+            selecnum14 = true
+            valores [incpos] = SelCart [13]
+            incpos++
+        }
+    }
+}
+function mudacor15(tipo){
+    if (!selecnum15){
+        document.getElementById("Num15").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_15"){
+            selecnum15 = true
+            valores [incpos] = SelCart [14]
+            incpos++
+        }
+    }
+}
+function mudacor16(tipo){
+    if (!selecnum16){
+        document.getElementById("Num16").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_16"){
+            selecnum16 = true
+            valores [incpos] = SelCart [15]
+            incpos++
+        }
+    }
+}function mudacor17(tipo){
+    if (!selecnum17){
+        document.getElementById("Num17").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_17"){
+            selecnum17 = true
+            valores [incpos] = SelCart [16]
+            incpos++
+        }
+    }
+}
+function mudacor18(tipo){
+    if (!selecnum18){
+        document.getElementById("Num18").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_18"){
+            selecnum18 = true
+            valores [incpos] = SelCart [17]
+            incpos++
+        }
+    }
+}
+function mudacor19(tipo){
+    if (!selecnum19){
+        document.getElementById("Num19").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_19"){
+            selecnum19 = true
+            valores [incpos] = SelCart [18]
+            incpos++
+        }
+    }
+}
+function mudacor20(tipo){
+    if (!selecnum20){
+        document.getElementById("Num20").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_20"){
+            selecnum20 = true
+            valores [incpos] = SelCart [19]
+            incpos++
+        }
+    }
+}
+function mudacor21(tipo){
+    if (!selecnum21){
+        document.getElementById("Num21").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_21"){
+            selecnum21 = true
+            valores [incpos] = SelCart [20]
+            incpos++
+        }
+    }
+}
+function mudacor22(tipo){
+    if (!selecnum22){
+        document.getElementById("Num22").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_22"){
+            selecnum22 = true
+            valores [incpos] = SelCart [21]
+            incpos++
+        }
+    }
+}
+function mudacor23(tipo){
+    if (!selecnum23){
+        document.getElementById("Num23").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_23"){
+            selecnum23 = true
+            valores [incpos] = SelCart [22]
+            incpos++
+        }
+    }
+}
+function mudacor24(tipo){
+    if (!selecnum24){
+        document.getElementById("Num24").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_24"){
+            selecnum24 = true
+            valores [incpos] = SelCart [23]
+            incpos++
+        }
+    }
+}
+function mudacor25(tipo){
+    if (!selecnum25){
+        document.getElementById("Num25").src= "_imagens/" + tipo + ".png"
+        if (tipo == "Numvm LF_25"){
+            selecnum25 = true
+            valores [incpos] = SelCart [24]
+            incpos++
+        }
+    }
+}
+function Apagar(){
+    if (valores.length == 0) {
+        window.alert('Selecione algum número antes de apagar!')
+    }else{
+        var limpaselecionados = document.getElementById("selNum")
+        var limpasoma = document.getElementById("soma")
+        var limparepetir = document.getElementById ("Confere")
+        limpaselecionados.style.color = "#000"
+        limpaselecionados.innerHTML = "<p>Números selecionados</p>"
+        limpasoma.style.color = "#000"
+        limpasoma.innerHTML = "<p>Soma dos números selecionados:</p>"
+        limparepetir.style.color = "#000"
+        limparepetir.innerHTML = "<p>Repetições em concursos anteriores:</p>"
+        var numcart = valores[incpos-1] /*numcart = número do cartão*/
+        valores.splice(incpos-1,numcart)
+        switch (numcart){
+            case 1:
+                selecnum1 = false
+                mudacor1('Numpt LF_01')
+            break
+
+            case 2:
+                selecnum2 = false
+                mudacor2('Numpt LF_02')
+            break
+
+            case 3:
+                selecnum3 = false
+                mudacor3('Numpt LF_03')
+            break
+
+            case 4:
+                selecnum4 = false
+                mudacor4('Numpt LF_04')
+            break
+
+            case 5:
+                selecnum5 = false
+                mudacor5('Numpt LF_05')
+            break
+
+            case 6:
+                selecnum6 = false
+                mudacor6('Numpt LF_06')
+            break
+
+            case 7:
+                selecnum7 = false
+                mudacor7('Numpt LF_07')
+            break
+
+            case 8:
+                selecnum8 = false
+                mudacor8('Numpt LF_08')
+            break
+
+            case 9:
+                selecnum9 = false
+                mudacor9('Numpt LF_09')
+            break
+
+            case 10:
+                selecnum10 = false
+                mudacor10('Numpt LF_10')
+            break
+
+            case 11:
+                selecnum11 = false
+                mudacor11('Numpt LF_11')
+            break
+
+            case 12:
+                selecnum12 = false
+                mudacor12('Numpt LF_12')
+            break
+
+            case 13:
+                selecnum13 = false
+                mudacor13('Numpt LF_13')
+            break
+
+            case 14:
+                selecnum14 = false
+                mudacor14('Numpt LF_14')
+            break
+
+            case 15:
+                selecnum15 = false
+                mudacor15('Numpt LF_15')
+            break
+
+            case 16:
+                selecnum16 = false
+                mudacor16('Numpt LF_16')
+            break
+
+            case 17:
+                selecnum17 = false
+                mudacor17('Numpt LF_17')
+            break
+
+            case 18:
+                selecnum18 = false
+                mudacor18('Numpt LF_18')
+            break
+
+            case 19:
+                selecnum19 = false
+                mudacor19('Numpt LF_19')
+            break
+
+            case 20:
+                selecnum20 = false
+                mudacor20('Numpt LF_20')
+            break
+            
+            case 21:
+                selecnum21 = false
+                mudacor21('Numpt LF_21')
+            break
+            
+            case 22:
+                selecnum22 = false
+                mudacor22('Numpt LF_22')
+            break
+            
+            case 23:
+                selecnum23 = false
+                mudacor23('Numpt LF_23')
+            break
+            
+            case 24:
+                selecnum24 = false
+                mudacor24('Numpt LF_24')
+            break
+            
+            case 25:
+                selecnum25 = false
+                mudacor25('Numpt LF_25')
+            break
+
+            default:
+            break
+        }
+        incpos--
+    }
+}
+/*ERRO: FORA DA FUNÇÃO ->> var res = document.getElementById("selNum")*/
+function Verificar() {
+    var res = document.getElementById("selNum")
+    if (valores.length == 0 || valores.length > 15) {
+        if (valores.length == 0)
+        window.alert('Selecione os números antes de verificar!')
+        if (valores.length > 15)
+            window.alert('Selecione no máximo 15 números!')
+    }else {
+        res.style.color="#00f"
+        /*res.innerHTML = ''*/
+        res.innerHTML = `<p>Os ${incpos} valores selecionados são:<br/>
+        <p>${valores}</p></p>`
+        /*document.write("TESTE")*/
+    }
+} 
+/*
+var jogos = [
+[18,20,25,23,10,11,24,14,6,2,13,9,5,16,3],
+[23,15,5,4,12,16,20,6,11,19,24,1,9,13,7],
+[20,23,12,8,6,1,7,11,14,4,16,10,9,17,24],
+[16,5,25,24,23,8,12,2,17,18,1,10,4,19,13],
+[15,13,20,2,11,24,9,16,4,23,25,12,8,19,1],
+[23,19,1,5,7,21,16,10,15,25,6,2,12,4,17],
+[22,4,15,8,16,14,21,23,12,1,25,19,7,10,18],
+[19,16,18,9,13,8,5,25,17,10,6,15,1,22,20],
+[21,4,17,5,3,13,16,9,20,24,25,19,11,15,10],
+[24,19,8,23,6,2,20,11,9,3,4,10,5,12,14]
+]*/
+function Calcular(){
+    var somanum = 0
+    var Concursos = 0
+    var pontos = 0
+    var verfJogo = 0
+    var ponto11 = 0
+    var ponto12 = 0
+    var ponto13 = 0
+    var ponto14 = 0
+    var ponto15 = 0
+    var Lin = 0
+    var Col = 0
+    var confjogo = document.getElementById("Confere")
+    var somatotal = document.getElementById("soma")
+
+    if (valores.length == 0 || valores.length > 15) {
+        if (valores.length == 0)
+            window.alert('Selecione os números antes de de cálcular!')
+        if (valores.length > 15)
+            window.alert('Selecione no máximo 15 números')
+    }else {
+        /*window.alert('Teste calculando!!!')*/
+        for (var pos in valores){
+            somanum += valores[pos]
+        }
+        somatotal.style.color="#00f"
+        somatotal.innerHTML = `<p>Soma dos números selecionados é: ${somanum}</p>`
+
+        for (Lin = 0; Lin < jogos.length;Lin++){
+            for (Col = 0; Col < 15; Col++){
+                for (var posX in valores){
+                    verfJogo = jogos [Lin][Col] - valores [posX]
+                    if (verfJogo == 0){
+                        pontos++
+                    }
+                } 
+            }
+            /*Verifica */
+            switch (pontos){
+                case 11: ponto11++;
+                break;
+                case 12: ponto12++;
+                break;
+                case 13: ponto13++;
+                break;
+                case 14: ponto14++;
+                break;
+                case 15: ponto15++;
+                break;
+                default:
+                break;
+            }
+            pontos = 0
+        }
+        /*Concursos = jogos.length*/
+
+        
+        if (valores.length <= 14){
+            confjogo.style.color = "#f00"
+            confjogo.innerHTML += `<p>Selecionar menos de 15 dezenas</br></p><p>invalidam números de repetições.</br</p>`
+        }
+        if (ponto11 != 0){
+            confjogo.style.color = "#00f"
+            confjogo.innerHTML += `<p>11 pontos em ${ponto11} jogos.</br></p>`
+        }
+        if (ponto12 != 0){
+            confjogo.style.color = "#00f"
+            confjogo.innerHTML += `<p>12 pontos em ${ponto12} jogos.</br</p>`
+        }
+        if (ponto13 != 0){
+            confjogo.style.color = "#00f"
+            confjogo.innerHTML += `<p>13 pontos em ${ponto13} jogos.</br</p>`
+        }
+        if (ponto14 != 0){
+            confjogo.style.color = "#00f"
+            confjogo.innerHTML += `<p>14 pontos em ${ponto14} jogos.</br</p>`
+        }
+        if (ponto15 != 0){
+            confjogo.style.color = "#00f"
+            confjogo.innerHTML += `<p>15 pontos em ${ponto15} jogos.</br</p>`
+        }
+        
+        ponto11 = 0
+        ponto12 = 0
+        ponto13 = 0
+        ponto14 = 0
+        ponto15 = 0
+    }
+} 
+/* shift+Alt e selecione no início (com a sete) para retornar o TAB com o DEL*/   
